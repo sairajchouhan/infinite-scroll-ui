af5ed8e6b72c809a34e9f5fe92c914ce33647015
Shahid Iqbal Khan <mindmap2app@gmail.com>
2021-11-25 20:18:18 +0500

    next js infinie scroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,64 @@
-import React, { useEffect } from 'react'
-import { useInfiniteQuery } from 'react-query'
-import axios from 'axios'
-import { useInView } from 'react-intersection-observer'
+import React, { useEffect } from 'react';
+import { useInfiniteQuery } from 'react-query';
+import axios from 'axios';
+import { useInView } from 'react-intersection-observer';
 
 export default function Home() {
-  const { ref, inView } = useInView()
+	const { ref, inView } = useInView();
 
-  const { isLoading, isError, data, error, isFetchingNextPage, fetchNextPage, hasNextPage } =
-    useInfiniteQuery(
-      'posts',
-      async ({ pageParam = '' }) => {
-        await new Promise((res) => setTimeout(res, 1000))
-        const res = await axios.get('/api/post?cursor=' + pageParam)
-        return res.data
-      },
-      {
-        getNextPageParam: (lastPage) => lastPage.nextId ?? false,
-      }
-    )
+	const {
+		isLoading,
+		isError,
+		data,
+		error,
+		isFetchingNextPage,
+		fetchNextPage,
+		hasNextPage,
+	} = useInfiniteQuery(
+		'posts',
+		async ({ pageParam = '' }) => {
+			await new Promise((res) => setTimeout(res, 1000));
+			const res = await axios.get('/api/post?cursor=' + pageParam);
+			return res.data;
+		},
+		{
+			getNextPageParam: (lastPage) => lastPage.nextId ?? false,
+		}
+	);
 
-  useEffect(() => {
-    if (inView && hasNextPage) {
-      fetchNextPage()
-    }
-  }, [inView])
+	useEffect(() => {
+		if (inView && hasNextPage) {
+			fetchNextPage();
+		}
+	}, [inView]);
 
-  if (isLoading) return <div className="loading">Loading...</div>
-  if (isError) return <div>Error! {JSON.stringify(error)}</div>
+	if (isLoading) return <div className='loading'>Loading...</div>;
+	if (isError) return <div>Error! {JSON.stringify(error)}</div>;
 
-  return (
-    <div className="container">
-      {data &&
-        data.pages.map((page) => {
-          return (
-            <React.Fragment key={page.nextId ?? 'lastPage'}>
-              {page.posts.map((post: { id: number; title: string; createdAt: Date }) => (
-                <div className="post" key={post.id}>
-                  <p>{post.id}</p>
-                  <p>{post.title}</p>
-                  <p>{post.createdAt}</p>
-                </div>
-              ))}
-            </React.Fragment>
-          )
-        })}
+	return (
+		<div className='container'>
+			{data &&
+				data.pages.map((page) => {
+					return (
+						<React.Fragment key={page.nextId ?? 'lastPage'}>
+							{page.posts.map(
+								(post: { id: number; title: string; createdAt: Date }) => (
+									<div className='post' key={post.id}>
+										<p>{post.id}</p>
+										<p>{post.title}</p>
+										<p>{post.createdAt}</p>
+									</div>
+								)
+							)}
+						</React.Fragment>
+					);
+				})}
 
-      {isFetchingNextPage ? <div className="loading">Loading...</div> : null}
+			{isFetchingNextPage ? <div className='loading'>Loading...</div> : null}
 
-      <span style={{ visibility: 'hidden' }} ref={ref}>
-        intersection observer marker
-      </span>
-    </div>
-  )
+			<span style={{ visibility: 'hidden' }} ref={ref}>
+				intersection observer marker
+			</span>
+		</div>
+	);
 }
--- prisma/migrations/20210920052532_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Post" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
-);
